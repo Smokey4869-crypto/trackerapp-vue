ece2f7160b25afc526381160baa68f7cb83d0b50
Lemoonh <anhminhnguyenle291@gmail.com>
2022-12-31 08:36:29 +1100

    commit header component

---
+++ src/components/Button.vue
@@ -0,0 +1,21 @@
+<template>
+    <button :style="{background: color }" class="btn">{{ text }}</button>
+</template>
+
+<script lang="ts">  
+
+import { Options, Vue } from 'vue-class-component'
+
+@Options({
+    props: {
+        text: String,
+        color: String
+    }
+})
+
+export default class Button extends Vue {
+    text!: string
+    color!: string
+}
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Button from "./Button.vue";
+
+@Options({
+  props: {
+    title: {
+      type: String,
+      default: "Hello World"
+    }
+  },
+  components: {
+    Button,
+  }
+})
+export default class HeaderComponent extends Vue {
+  title!: string
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
